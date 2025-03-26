7f334ce3b0f7395d814f900cabdd4360f4c283fd
daalagelerua <aurele284@gmail.com>
2025-03-26 23:34:24 +0100

    another class with 2 methods

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -29,7 +29,7 @@ export default class pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency})`;
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
