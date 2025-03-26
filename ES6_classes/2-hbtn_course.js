94475ee3c101cfbf1a8098e8faa66bef99267b7c
daalagelerua <aurele284@gmail.com>
2025-03-26 22:46:02 +0100

    classe with getters and setters

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,13 +1,13 @@
 class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== "string") {
-      throw new Error("name must be a string");
+      throw new TypeError("name must be a string");
     }
-    if (typeof length !== "number") {
-      throw new Error("length must be a number");
+    if (typeof length !== "number" || length < 0) {
+      throw new TypeError("length must be a positive number");
     }
     if (!Array.isArray(students) || !students.every(student => typeof student === "string")) {
-      throw new Error("students must be an array of strings")
+      throw new TypeError("students must be an array of strings")
     }
 
     this._name = name;
@@ -29,8 +29,8 @@ class HolbertonCourse {
     return this._length;
   }
   set length(value) {
-    if (typeof length !== "number") {
-      throw new Error("length must be a number");
+    if (typeof value !== "number" || value < 0) {
+      throw new TypeError("length must be a positive number");
     }
     this._length = value;
   }
