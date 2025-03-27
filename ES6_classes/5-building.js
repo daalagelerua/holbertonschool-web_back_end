93282daf467ad785d994647df2e6c3ed6c9df65e
daalagelerua <aurele284@gmail.com>
2025-03-27 13:24:50 +0100

    sqft

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -5,7 +5,7 @@ export default class Building {
       === Building.prototype.evacuationWarningMessage) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-    
+
     this._sqft = sqft;
   }
 
