0af764f6fa40fdd885809e5053da5981e45f9411
daalagelerua <aurele284@gmail.com>
2025-04-03 01:09:56 +0200

    promise.all

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then(([uploadPhoto, user]) => {
       console.log(`${uploadPhoto.body} ${user.firstName} ${user.lastName}`);
     })
