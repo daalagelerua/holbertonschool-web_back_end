ba0743e8fc8ca0dabf4dd1d5df11e3c6b4484cc2
daalagelerua <aurele284@gmail.com>
2025-04-03 02:05:22 +0200

    multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -8,7 +8,7 @@ async function handleProfileSignup(firstName, lastName, fileName) {
   ]);
   return result.map((result) => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
+    value: result.status === 'fulfilled' ? result.value : `Error: ${result.reason.message}`,
   }));
 }
 
