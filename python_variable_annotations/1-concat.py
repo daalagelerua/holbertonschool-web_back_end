91c8b1326907d9504b0bc06b8653681857ffcd74
daalagelerua <aurele284@gmail.com>
2025-04-23 13:44:54 +0200

    concat 2 string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module is used to concatenate 2 strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenate two strings and return the result."""
+    return str1 + str2
