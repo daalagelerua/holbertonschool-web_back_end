be4ba3482e5fe10a94a89e63a3dab7c07954f44d
daalagelerua <aurele284@gmail.com>
2025-04-23 15:39:21 +0200

    multiply

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module create a function that multiplies a float by a given
+multiplier.
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Create a function that multiplies a float by a given multiplier."""
+    def multiply(n: float) -> float:
+        return n * multiplier
+    
+    return multiply
