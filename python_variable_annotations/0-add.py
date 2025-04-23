99f9e9ae89497ad5247f91d0722c33baa3af6a05
daalagelerua <aurele284@gmail.com>
2025-04-23 13:38:34 +0200

    typed function to sum float numbers

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+This module provides basic addition operation
+"""
+
+
 def add(a: float, b: float) -> float:
     """Add two floating point numbers and return their sum."""
     return a + b
