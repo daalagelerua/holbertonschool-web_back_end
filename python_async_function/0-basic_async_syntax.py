41bde50d1c7dfdf48188d3b4cb95f2b1860225b1
daalagelerua <aurele284@gmail.com>
2025-04-25 10:04:43 +0200

    random time

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module wait a random time between 0 and 10 seconds
+and returns the number of seconds wited as a float
+"""
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Wait a random time between 0 and max_delay secondes and return that value"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
