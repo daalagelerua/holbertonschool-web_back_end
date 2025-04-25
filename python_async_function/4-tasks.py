496ee190001d8a16197c9abba74e5fb6edab7de2
daalagelerua <aurele284@gmail.com>
2025-04-25 12:37:24 +0200

    task delay

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -3,17 +3,21 @@
 This module execute task_wait_random n times with a specified delay
 """
 import asyncio
-from typing import list
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Execute task_wait_random n times with specified max_delay"""
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-
     delays = []
-    # Await in comprehension can be problematic => hence below
-    for future in asyncio.as_completed(tasks):
-        result = await future
-        delays.append(result)
+    
+    async def append_delay():
+        task = task_wait_random(max_delay)
+        delay = await task
+        delays.append(delay)
+        return delay
+    
+    tasks = [append_delay() for _ in range(n)]
+
+    await asyncio.gather(*tasks)
 
     return delays
