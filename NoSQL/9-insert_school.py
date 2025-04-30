674c5eb29e91fa6e34bd3a8abf1a0fa5d1a58296
daalagelerua <aurele284@gmail.com>
2025-04-30 15:42:31 +0200

    insert new doc based on kwargs

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""
+This module insert a new document in a collection
+based on kwargs.
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    inserts a new document in a collection based on kwargs.
+    return the new _id
+    """
+    if mongo_collection is None:
+        return None
+    
+    result = mongo_collection.insert_one(kwargs)
+
+    return result.inserted_id
