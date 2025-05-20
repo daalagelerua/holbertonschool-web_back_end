b86e0d5b3d89efd348c9b7ec5ed3c1bdb23eb566
daalagelerua <aurele284@gmail.com>
2025-05-20 13:10:56 +0200

    lint

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -8,38 +8,38 @@ function countStudents(path) {
   try {
     // Try to read the file synchronously
     const fileContent = fs.readFileSync(path, 'utf8');
-    
+
     // Split content by lines and filter out empty lines
     const lines = fileContent.split('\n').filter((line) => line.trim() !== '');
-    
+
     // Extract headers (first line)
     const headers = lines[0].split(',');
-    
+
     // Get index of field column
     const fieldIndex = headers.indexOf('field');
     const firstnameIndex = headers.indexOf('firstname');
-    
+
     // Remove header line to get only student data
     const studentLines = lines.slice(1);
-    
+
     // Log total number of students
     console.log(`Number of students: ${studentLines.length}`);
-    
+
     // Group students by field
     const studentsByField = {};
-    
+
     studentLines.forEach((line) => {
       const fields = line.split(',');
       const field = fields[fieldIndex];
       const firstname = fields[firstnameIndex];
-      
+
       if (!studentsByField[field]) {
         studentsByField[field] = [];
       }
-      
+
       studentsByField[field].push(firstname);
     });
-    
+
     // Log number of students in each field
     for (const field in studentsByField) {
       if (Object.prototype.hasOwnProperty.call(studentsByField, field)) {
@@ -47,7 +47,6 @@ function countStudents(path) {
         console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
       }
     }
-    
   } catch (error) {
     throw new Error('Cannot load the database');
   }
