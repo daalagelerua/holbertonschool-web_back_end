6ae0b83c46d31cb3a3d7fe4eed868b1e7a84e486
daalagelerua <aurele284@gmail.com>
2025-05-23 10:48:42 +0200

    full server lint

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,48 +1,46 @@
 import fs from 'fs';
 
-const readDatabase = (filePath) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (error, data) => {
-      if (error) {
-        reject(error);
-        return;
-      }
+const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf8', (error, data) => {
+    if (error) {
+      reject(error);
+      return;
+    }
 
-      try {
-        // Split content by lines and filter out empty lines
-        const lines = data.split('\n').filter((line) => line.trim() !== '');
+    try {
+      // Split content by lines and filter out empty lines
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
 
-        // Extract headers (first line)
-        const headers = lines[0].split(',');
+      // Extract headers (first line)
+      const headers = lines[0].split(',');
 
-        // Get index of field column
-        const fieldIndex = headers.indexOf('field');
-        const firstnameIndex = headers.indexOf('firstname');
+      // Get index of field column
+      const fieldIndex = headers.indexOf('field');
+      const firstnameIndex = headers.indexOf('firstname');
 
-        // Remove header line to get only student data
-        const studentLines = lines.slice(1);
+      // Remove header line to get only student data
+      const studentLines = lines.slice(1);
 
-        // Group students by field
-        const studentsByField = {};
+      // Group students by field
+      const studentsByField = {};
 
-        studentLines.forEach((line) => {
-          const fields = line.split(',');
-          const field = fields[fieldIndex];
-          const firstname = fields[firstnameIndex];
+      studentLines.forEach((line) => {
+        const fields = line.split(',');
+        const field = fields[fieldIndex];
+        const firstname = fields[firstnameIndex];
 
-          if (!studentsByField[field]) {
-            studentsByField[field] = [];
-          }
+        if (!studentsByField[field]) {
+          studentsByField[field] = [];
+        }
 
-          studentsByField[field].push(firstname);
-        });
+        studentsByField[field].push(firstname);
+      });
 
-        resolve(studentsByField);
-      } catch (parseError) {
-        reject(parseError);
-      }
-    });
+      resolve(studentsByField);
+    } catch (parseError) {
+      reject(parseError);
+    }
   });
-};
+});
 
 export default readDatabase;
